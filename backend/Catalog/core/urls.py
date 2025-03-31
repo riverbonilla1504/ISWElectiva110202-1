408da6a2ecead58926b7652f10045c8f373a6303
T0m4s1n <tomas.benavidesc@campusucc.edu.co>
2025-03-31 15:54:36 -0500

    added views and urls

--- backend/Catalog/core/urls.py
+++ backend/Catalog/core/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+
+from core.views import AllProductsView, CreateProductView, DeleteProductView, ChangeProductAvailabilityView
+
+urlpatterns = [
+    path('', AllProductsView.as_view(), name='all_products'),
+    path('', CreateProductView.as_view(), name='create_product'),
+    path('<int:product_id>', DeleteProductView.as_view(), name='delete_product'),
+    path('change_availability/<int:product_id>', ChangeProductAvailabilityView.as_view(), name='change_product_availability'),
+]
--- backend/Catalog/core/views.py
+++ backend/Catalog/core/views.py
@@ -4,3 +4,35 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+class AllProductsView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class CreateProductView(APIView):
+    def post(self, request):
+        name = request.data.get('name')
+        description = request.data.get('description')
+        price = request.data.get('price')
+        picture = request.data.get('picture')
+        product = Product.objects.create(name=name, description=description, price=price, picture=picture)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=201)
+
+class DeleteProductView(APIView):
+    def delete(self, request, product_id):
+        try:
+            product = Product.objects.delete_product(product_id)
+            return Response( {"deleted product with id ": f"{product_id}"}, status=200)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not Found"}, status=404)
+
+class ChangeProductAvailabilityView(APIView):
+    def patch(self, request, product_id):
+        try:
+            product = Product.objects.change_availability(product_id) 
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status=200)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not Found"}, status=404)
