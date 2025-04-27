80956cfeca20b9068cbe9d42bd682d990653bf6f
river PC <river.bonilla@campusucc.edu.co>
2025-04-27 10:31:27 -0500

    added endpoint

--- backend/User/core/urls.py
+++ backend/User/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from core.views import RegisterView, LoginView, EditUserView
+from core.views import RegisterView, LoginView, EditUserView, getUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('edit/<int:id_user>/', EditUserView.as_view(), name='edit_user'),
+    path('get/<int:id_user>/', getUserView.as_view(), name='get_user'),
 ]
