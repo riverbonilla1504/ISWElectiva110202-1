58c4d5aacfbdfd697a211aad8f9713962681558d
river PC <river.bonilla@campusucc.edu.co>
2025-04-27 10:33:25 -0500

    cors updated

--- backend/Catalog/Catalog/settings.py
+++ backend/Catalog/Catalog/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-@3#t8nl9m!hn*3*y86ke3=c683wosfuspep+p^x@dfgb!vd%u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
+]
 
 # Application definition
 
